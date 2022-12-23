2cc04c3ad5538c3d33d79a1b3017d233daf5dbe9
Phong <90952598+phongthanhh@users.noreply.github.com>
2022-12-23 21:13:10 +0700

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BTThucHanhLayout from './components/BTDay1/BTThucHanhLayout';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BTThucHanhLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BTDay1/BTThucHanhLayout.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
+
+export default class
+    extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Body />
+                <Footer />
+            </div>
+        )
+    }
+}
